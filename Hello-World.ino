3305e05cadf42f6e23fdc2964be240222096028b
markahofmeister <markhof12@gmail.com>
2022-10-10 11:51:36 -0400

    Added my Hello World code

---
+++ Hello-World.ino
@@ -0,0 +1,21 @@
+
+int buttonPin = 8;
+int LEDPin = 9;
+
+void setup() {
+
+  pinMode(buttonPin, INPUT);
+  pinMode(LEDPin, OUTPUT);
+
+}
+
+void loop() {
+
+  if (digitalRead(buttonPin) == LOW) {
+    digitalWrite(LEDPin, HIGH);
+    delay(500);
+    digitalWrite(LEDPin, LOW);
+    delay(500);
+  }
+
+}
